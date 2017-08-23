15d435edc30dc7303f2e6b7e2a9e0cd915be34f6
matcarthy <31219462+matcarthy@users.noreply.github.com>
2017-08-23 13:01:58 -0500

    Update GPD_PCA.R

--- GPD_PCA.R
+++ GPD_PCA.R
@@ -41,19 +41,26 @@ training12 <- predict(pca12, training12)
 test12 <- predict(pca12, test12)
 
 set.seed(123)
-split14 <- sample.split(gpd14.pca1$tot.trust.binary , SplitRatio = 0.8)
-training14 <- subset(gpd14.pca1, split14 == TRUE)
-test14 <- subset(gpd14.pca1, split14 == FALSE)
+split14.1 <- sample.split(gpd14.pca1$tot.trust.binary , SplitRatio = 0.8)
+training14.1 <- subset(gpd14.pca1, split14.1 == TRUE)
+test14.1 <- subset(gpd14.pca1, split14.1 == FALSE)
+
+set.seed(123)
+split14.2 <- sample.split(gpd14.pca2$tot.trust.binary , SplitRatio = 0.8)
+training14.2 <- subset(gpd14.pca2, split14.2 == TRUE)
+test14.2 <- subset(gpd14.pca2, split14.2 == FALSE)
 
 ##scale
-training14[-60] <- scale(training14[-60])
-test14[-60] <- scale(test14[-60])
+#training14[-60] <- scale(training14[-60])
+#test14[-60] <- scale(test14[-60])
 
 ##pca
 
-pca14 <- preProcess(x = training14[-60], method = 'pca', pcaComp = 2)
-training14 <- predict(pca14, training14)
-test14 <- predict(pca14, test14)
+pca14.1 <- preProcess(x = training14.1[-60], method = 'pca', pcaComp = 2)
+training14.1 <- predict(pca14.1, training14.1)
+test14.1 <- predict(pca14.1, test14.1)
 
-
+pca14.2 <- preProcess(x = training14.2[-33], method = 'pca', pcaComp = 2)
+training14.2 <- predict(pca14.2, training14.2)
+test14.2 <- predict(pca14.2, test14.2)
 
